8981e4809ff2d4671b1af3d64b1f357c8686ca73
Eloise Holland <eloise.holland@Mac.Home>
2025-02-12 12:49:02 +0000

    removed typos

--- src/test/scala/week2wed/FizzBuzzSpec.scala
+++ src/test/scala/week2wed/FizzBuzzSpec.scala
@@ -8,23 +8,23 @@ class FizzBuzzSpec extends AnyFlatSpec{
 
   val fb: FizzBuzz = new FizzBuzz
 
-  "FizzBuzz" should "return FizzBuzz for multiples of 15" in {
+  "fizzBuzz" should "return FizzBuzz for multiples of 15" in {
     assert(fb.fizzBuzz(15) == "FizzBuzz")
   }
 
-  "FizzBuzz" should "return Fizz for multiples of 3" in {
+  "fizzBuzz" should "return Fizz for multiples of 3" in {
     assert(fb.fizzBuzz(3) == "Fizz")
   }
 
-  "FizzBuzz" should "return Buzz for multiples of 5" in {
+  "fizzBuzz" should "return Buzz for multiples of 5" in {
     assert(fb.fizzBuzz(5) == "Buzz")
   }
 
-  "FizzBuzz" should "return input x as string for anything else" in {
+  "fizzBuzz" should "return input x as string for anything else" in {
     assert(fb.fizzBuzz(2) == "2")
   }
 
-  "FizzBuzz" should "return \"0\" for 0" in {
+  "fizzBuzz" should "return \"0\" for 0" in {
     assert(fb.fizzBuzz(0) == "0")
   }
 
