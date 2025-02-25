f05e39072076846e35169a4b12bc7d6ac2099ac3
Eloise Holland <eloise.holland@Mac.Home>
2025-02-25 10:49:46 +0000

    improved on fibonacci (a lot)

--- src/main/scala/week4/mon/afternoonTasks/Fibonacci.scala
+++ src/main/scala/week4/mon/afternoonTasks/Fibonacci.scala
@@ -1,5 +1,7 @@
 package week4.mon.afternoonTasks
 
+import scala.annotation.tailrec
+
 class Fibonacci {
   def betterFibonacci(n: Int, acc: Int = 1): Option[Int] = {
     if (n < 0) None
@@ -7,4 +9,26 @@ class Fibonacci {
     else if (n <= 2) Some(acc)
     else betterFibonacci(n-1, acc + betterFibonacci(n-2).get)
   }
+
+  //Lan Fibonacci
+  def fibonacciRecursion(position: Int): Int = {
+    @tailrec
+    def fibonacciRecursionHelper(baseCountdown: Int, leftNum: Int, rightNum: Int, fibList: List[Int]): Int = {
+      if (baseCountdown <= 1) leftNum
+      else {
+        println(s"$baseCountdown, $leftNum, $rightNum, $fibList")
+        fibonacciRecursionHelper( baseCountdown - 1, rightNum, leftNum + rightNum, fibList :+ leftNum)
+      }
+    }
+    fibonacciRecursionHelper(position, 0, 1, Nil)
+  }
+
+  //My mash-up
+  def evenBetterFibonacci(n: Int, firstAcc: Long = 1, secondAcc: Long = 1): Either[String, Long] = n match {
+    case n if n < 0 => Left(s"$n is not a valid index for this sequence.")
+    case 0 => Right(0)
+    case 1 => Right(firstAcc)
+    case _ => evenBetterFibonacci(n-1, secondAcc, firstAcc + secondAcc)
+  }
 }
+
--- src/test/scala/week4mon/FibonacciSpec.scala
+++ src/test/scala/week4mon/FibonacciSpec.scala
@@ -1,14 +1,13 @@
 package week4mon
 
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import week4.mon.afternoonTasks.Fibonacci
 
 class FibonacciSpec extends AnyWordSpec with Matchers {
+  val fib = new Fibonacci
 
-  "fibonacci" should {
-    val fib = new Fibonacci
+  "betterFibonacci" should {
 
     "return nth fibonacci number with valid input" in {
     assert(fib.betterFibonacci(3).get == 2)
@@ -26,6 +25,27 @@ class FibonacciSpec extends AnyWordSpec with Matchers {
       assert(fib.betterFibonacci(-1).isEmpty)
       assert(fib.betterFibonacci(-3).isEmpty)
       assert(fib.betterFibonacci(-10).isEmpty)
+    }
+  }
+
+  "evenBetterFibonacci" should {
+
+    "return nth fibonacci number with valid input" in {
+    assert(fib.evenBetterFibonacci(3).fold(l => l, r => r) == 2)
+    assert(fib.evenBetterFibonacci(4).fold(l => l, r => r) == 3)
+    assert(fib.evenBetterFibonacci(5).fold(l => l, r => r) == 5)
+    assert(fib.evenBetterFibonacci(6).fold(l => l, r => r) == 8)
+    assert(fib.evenBetterFibonacci(10).fold(l => l, r => r) == 55)
+    }
+
+    "return 0 with 0 input" in {
+      assert(fib.evenBetterFibonacci(0).fold(l => l, r => r) == 0)
+    }
+
+    "return None with negative input" in {
+      assert(fib.evenBetterFibonacci(-1).fold(l => l, r => r) == "-1 is not a valid index for this sequence.")
+      assert(fib.evenBetterFibonacci(-3).fold(l => l, r => r) == "-3 is not a valid index for this sequence.")
+      assert(fib.evenBetterFibonacci(-10).fold(l => l, r => r) == "-10 is not a valid index for this sequence.")
     }
   }
 }
